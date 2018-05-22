f87315d84b0b93789b45beee239d2a5e1e179b47
Christoph Hessel <Christoph.Hessel@ovg.jm.rlp.de>
2018-05-22 13:22:00 +0200

    EgvpPostbox Model hinzugefügt

---
+++ EgvpEpFetcher/Models/EgvpPostbox.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OvgRlp.EgvpEpFetcher.Models
+{
+    public class EgvpPostbox
+    {
+        public String Id;
+
+        public String Name;
+
+        public List<String> ExportPath;
+    }
+}
