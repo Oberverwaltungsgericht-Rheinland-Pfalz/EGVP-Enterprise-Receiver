1352602680a687b53578772278de0f0d63717aa7
Christoph Hessel <Christoph.Hessel@ovg.jm.rlp.de>
2021-06-07 09:55:26 +0200

    XPathDepartmentId in PostboxSettings aufgenommen
    #10

--- src/OvgRlp.EgvpEpReceiver.Infrastructure/Models/PostboxSettings/ReceiveDepartments.cs
+++ src/OvgRlp.EgvpEpReceiver.Infrastructure/Models/PostboxSettings/ReceiveDepartments.cs
@@ -10,6 +10,8 @@ namespace OvgRlp.EgvpEpReceiver.Infrastructure.Models
   {
     public string LogLevelDepartmentNotFound { get; set; }
 
+    public string XPathDepartmentId { get; set; }
+
     public List<Department> Departments { get; set; }
 
     public ReceiveDepartments()
