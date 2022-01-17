2cc7a7d65e344f12fae23ed2ac43f8ae604adca2
Christoph Hessel <Christoph.Hessel@ovg.jm.rlp.de>
2022-01-17 16:54:05 +0100

    nUnit-Tests: XJustizService Tests hinzugefügt
    #11

---
+++ src/OvgRlp.EgvpEpReceiver.Test/XJustizService_Test.cs
@@ -0,0 +1,34 @@
+﻿using OvgRlp.EgvpEpReceiver.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OvgRlp.EgvpEpReceiver.Test
+{
+  public class XJustizService_Test
+  {
+    [Fact]
+    public void Message_DetermineXJustizText_OK()
+    {
+      byte[] message = Helper.GetMessageAsZip("Testnachricht_1");
+      Assert.Contains("http://www.xjustiz.de", XJustizService.GetXjustizXmlTextFromZipArchive(message));
+    }
+
+    [Fact]
+    public void Message_IsNotEeb()
+    {
+      byte[] message = Helper.GetMessageAsZip("Testnachricht_1");
+      Assert.False(XJustizService.IsEeb(message));
+    }
+
+    [Fact]
+    public void Message_IsEeb()
+    {
+      byte[] message = Helper.GetMessageAsZip("TestnachrichtEeb_1");
+      Assert.True(XJustizService.IsEeb(message));
+    }
+  }
+}
